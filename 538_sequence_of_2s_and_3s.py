359c66a2dcdbd606ac90d9ec967ca3bdb50b9159
zsegel <zachary.segel@gmail.com>
2017-08-18 21:50:30 -0400

    Update 538_sequence_of_2s_and_3s.py

--- 538_sequence_of_2s_and_3s.py
+++ 538_sequence_of_2s_and_3s.py
@@ -1,6 +1,6 @@
 '''
 Solution to the Riddler Classic problem found here:
-
+https://fivethirtyeight.com/features/can-you-unravel-these-number-strings/
 '''
 
 # We can use the first four entries in the sequence to generate as many more as we please.
@@ -21,9 +21,9 @@ threes = len(sequence) - twos
 # And calculate the ratio of threes to twos:
 print float(threes) / twos
 
-
 '''
+FINDINGS:
 As the length of the sequence approaches infinity, the ratio of threes to twos in the 
 sequence approaches sqrt(3) + 1. There might be an interesting reason why this is the 
-case, but I don't know what that reason is. 
+case, but I don't know what that reason is.
 '''
