ec76b11965a026f12b074993cc3ad23638af9d19
zsegel <zachary.segel@gmail.com>
2017-09-01 15:11:57 -0400

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,2 +1,21 @@
 import itertools
 
+# N is the number of players
+N = 3
+
+arrangements = list(itertools.product(['red', 'green'], repeat=N))
+
+def strategy(info):
+  """takes info about other players' hats and returns a guess"""
+  if info.count('blue') == 0:
+    return 'blue'
+  elif info.count('red') == 0:
+    return 'red'
+  else:
+    return None
+  
+for a in arrangements:
+  guesses = []
+  for p in range(N):
+    
+  
