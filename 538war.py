beb841fc113f8ecade24ccdbe97f481445ceb79d
zsegel <zachary.segel@gmail.com>
2017-09-08 15:09:37 -0500

    Update 538war.py

--- 538war.py
+++ 538war.py
@@ -1,30 +1,29 @@
-
-'''
+"""
 Solution to Riddler Classic problem found here:
 https://fivethirtyeight.com/features/riddler-nation-goes-to-war/
 
 Consider a standard, two-player, 52-card game of War. If I start 
 with just the four aces, and you start with all 48 other cards, 
 randomly shuffled, what are your chances of winning?
-'''
+
+The code below models the game as specified here: http://www.bicyclecards.com/how-to-play/war/
+Any cards a player wins are added to the bottom of that player's pile in a random order."""
 
 import numpy as np
 from collections import deque
 
 def sim_one_game():
-    """returns True if player B wins"""
-
-    A = deque([14, 14, 14, 14])
+    """returns True if the player who doesn't start with the aces wins"""
     
+    # starting cards
+    A = deque([14, 14, 14, 14])
     B = deque(range(2,14) * 4)
     np.random.shuffle(B)
     
     while len(A) and len(B):
-        rounds += 1
         cards = [A.popleft(), B.popleft()]
         war = []
 
-        # war
         while (cards[0] == cards[1]):
             war.extend(cards[:])
             try:
@@ -33,12 +32,10 @@ def sim_one_game():
             except:
                 return bool(len(B))
      
-        # A wins
         if cards[0] > cards[1]:
             cards.extend(war)
             np.random.shuffle(cards)
             A.extend(cards[:])
-        # B wins
         else:
             cards.extend(war)
             np.random.shuffle(cards)
@@ -47,3 +44,10 @@ def sim_one_game():
     else:
         return bool(len(B))
 
+# Monte Carlo approximation of the probability that the non-ace player wins
+sims = 1000000
+wins = 0
+for i in range(sims):
+    if sim_one_game():
+        wins += 1
+print wins/float(sims)
