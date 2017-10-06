c46ebbbb09fd67362d2df2289e5b019ef46a7567
zsegel <zachary.segel@gmail.com>
2017-10-06 14:27:50 -0400

    Update gameshow.py

--- gameshow.py
+++ gameshow.py
@@ -3,4 +3,15 @@ import itertools
 boxes = [1, 2, 3, 4]
 names = ['A', 'B', 'C', 'D']
 
-name_orders = itertools.permutations(names)
+name_orders = list(itertools.permutations(names))
+individual_strategies = list(itertools.combinations(boxes, 2))
+all_strategies = list(itertools.product(individual_strategies, repeat=4))
+
+def eval_strategy(s):
+    wins = 0
+    for order in name_orders:
+        wins +=
+(np.sum([order.index(names[i]) in s[i] for i in range(4)]) == 4)
+                                   
+                                   
+                                   
