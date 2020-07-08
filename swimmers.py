d482d3e60a6a35121ab9448a27a53e5bd77d1598
zsegel <zachary.segel@gmail.com>
2020-07-08 11:07:38 -0400

    Update swimmers.py

--- swimmers.py
+++ swimmers.py
@@ -1,3 +1,23 @@
+"""
+Riddler Express (3 July 2020)
+
+It’s summertime and my local swimming pool, which has exactly five swimming lanes (and no general swim area), 
+may be opening in the coming weeks. It remains unclear what social distancing practices will be required, but 
+it’s quite possible that swimmers will not be allowed to occupy adjacent lanes.
+
+Under these guidelines, the pool could accommodate at most three swimmers — one each in the first, third and 
+fifth lanes.
+
+Suppose a queue of swimmers arrives at the pool when it opens at 9 a.m. One at a time, each person randomly 
+picks a lane from among the lanes that are available (i.e., the lane has no swimmer already and is not adjacent 
+to any lanes with swimmers), until no more lanes are available.
+
+At this point, what is the expected number of swimmers in the pool?
+
+Extra credit: Instead of five lanes, suppose there are N lanes. When no more lanes are available, what is the 
+expected number of swimmers in the pool?
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
@@ -20,6 +40,7 @@ def sim_one(N):
 
 
 def sim_many(N, sims=10000):
+    '''monte carlo simulation to estimate the expected number of swimmers, given N lanes'''
     results = []
     for i in range(sims):
         results.append(sim_one(N))
@@ -39,3 +60,11 @@ model.fit(X, y)
 
 print("slope:", model.coef_)
 print("intercept", model.intercept_)
+
+"""
+Answer: for five lanes, the expected number of swimmers is about 2.466...
+
+Extra credit: There's a linear relationship between the number of lanes, N,  and the expected
+number of swimmers. Roughly, xxpected number of swimmers = 0.432*N + 0.3
+
+"""
