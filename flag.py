5cdaf0e1148a75c1d9840d729f627eadba1e6c33
zsegel <zachary.segel@gmail.com>
2020-07-08 10:55:52 -0400

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,3 +1,8 @@
+"""
+Riddler Classic (3 July 2020)
+It just so happens that when N equals 50, N is twice a square and N+1 is a centered 
+pentagonal number. After 50, what is the next integer N with these properties?
+"""
 n = 5
 while True:
     cpn = (5*n**2 + 5*n + 2)/2 # generate next centered pentagonal number
@@ -8,3 +13,4 @@ while True:
     else:
         n += 1
 print(int(cpn-1))
+# Answer: 16200
